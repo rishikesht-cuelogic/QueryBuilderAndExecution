aef415da088bd0952d46c1a6395161bd77be4c45
nileshp-cuelogic <nilesh.patil@cuelogic.co.in>
2016-04-15 20:07:07 +0530

    Portable Data Access Layer

---
+++ QueryBuilder/QueryBuilderConsole/PortableDataAccessLayer/Classes/Configuration.cs
@@ -0,0 +1,63 @@
+﻿using System.Configuration;
+
+
+namespace PortableDataAccessLayer
+{
+    internal static class Configuration
+    {
+        const string DEFAULT_CONNECTION_KEY = "defaultConnection";
+
+        public static string DefaultConnection
+        {
+            get
+            {
+                return "";// ConfigurationManager.AppSettings[DEFAULT_CONNECTION_KEY];
+            }
+        }
+
+        //public static string ProviderName
+        //{
+        //    get
+        //    {
+        //        return ConfigurationManager.ConnectionStrings[DefaultConnection].ProviderName;
+        //    }
+        //}
+
+        private static string providerName;
+
+        public static string ProviderName
+        {
+            get { return providerName; }
+            set { providerName = value; }
+        }
+
+
+
+        //public static string ConnectionString
+        //{
+        //    get
+        //    {
+        //        return ConfigurationManager.ConnectionStrings[DefaultConnection].ConnectionString;
+        //    }
+        //}
+
+        private static string connectionString;
+
+        public static string ConnectionString
+        {
+            get { return connectionString; }
+            set { connectionString = value; }
+        }
+
+        public static string GetConnectionString(string connectionName)
+        {
+            return ConnectionString;
+        }
+
+        public static string GetProviderName(string connectionName)
+        {
+            return ProviderName;
+        }
+
+    }
+}
---
+++ QueryBuilder/QueryBuilderConsole/PortableDataAccessLayer/IDataAccess.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortableDataAccessLayer
+{
+    interface IDataAccess
+    {
+        object GetDataFromDatabase(string sqlSelectCommond, string[] TableNames = null, ProcReturnType ProcReturnType = ProcReturnType.JSON);
+        int SaveDataToDatabase(string sqlInsertCommond);
+    }
+}
